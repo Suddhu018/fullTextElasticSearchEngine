7e1620e32cccc83b6e5fb496b44784e6f4bbb48b
Suddhu018 <shekharsudhanshu018@gmail.com>
2024-10-31 20:22:57 +0530

    resolved the error of persistant connection

--- driverCode.cpp
+++ driverCode.cpp
@@ -5,25 +5,97 @@
 
 */
 #include <bits/stdc++.h>
-#include "preprocessingDoc.cpp" //to preprocess the data saved in the documents folder
-#include <boost/algorithm/string.hpp>// to use the features of the boost library
-#include <thread>// to use the multithreadig functionality of c++
+#include <boost/algorithm/string.hpp>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "preprocessingDoc.cpp"
 #include "query.cpp"
-using namespace std;
-using namespace boost::algorithm; // Use the correct namespace
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "No string argument provided" << endl;
-        return 1;
+using namespace std;
+using namespace boost::algorithm;
+
+bool isPreprocessed = false;
+
+// Preprocess the data (called only once)
+void initializePreprocessing() {
+    if (!isPreprocessed) {
+        preProcessTheData(); // Preprocess data once
+        isPreprocessed = true;
+        cout << "Data preprocessed successfully." << endl;
+    }
+}
+
+// Function to handle incoming queries
+void handleClient(int clientSocket) {
+    char buffer[1024] = {0};
+    int valread = read(clientSocket, buffer, 1024);
+    string inputString(buffer);
+
+    // Trim input and satisfy the query
+    inputString = boost::trim_copy(inputString);
+    if (!inputString.empty()) {
+        satisfyQuery(inputString); // Handle the query using the preprocessed data
+        string response = "Query processed successfully\n";
+        send(clientSocket, response.c_str(), response.length(), 0);
+    } else {
+        string error = "Empty query received\n";
+        send(clientSocket, error.c_str(), error.length(), 0);
     }
 
-    string inputString = argv[1];
+    close(clientSocket);
+}
 
-    preProcessTheData(); // received the tokens which is stemmed by common words and which are not stop words
-                         // and when the above function is called the inverted index is also already called and built
-    satisfyQuery(inputString); // pass the input string to satisfyQuery
-    cout << "This ran successfully" << endl;
+int main(int argc, char* argv[]) {
+    // Initialize the preprocessing
+    initializePreprocessing();
+
+    // Create socket
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+}
+
+    // Define server address
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080); // Port for listening
+
+    // Bind the socket to the address
+    if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+}
+    // Start listening for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Server is running and waiting for queries on port 8080..." << endl;
+
+    // Accept and handle incoming connections
+    while (true) {
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // Handle the client's query
+        handleClient(new_socket);
+    }
 
     return 0;
 }
--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require("express");
-const { exec } = require("child_process");
+const net = require("net");
 
 const app = express();
 const port = 3000;
 
-// Route to execute C++ program on GET request
-app.get("/", (req, res) => {
-  res.send("Hello World I am server");
-});
+// Route to execute search query
 app.get("/search", (req, res) => {
-  // The string to pass to the C++ program
   const query = req.query.queryWord;
-  // Execute the C++ program with the string as an argument
-  exec(`./driverCode ${query}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing program: ${error.message}`);
-      return res.status(500).send(`Error: ${error.message}`);
-    }
-    if (stderr) {
-      console.error(`Program stderr: ${stderr}`);
-      return res.status(500).send(`Stderr: ${stderr}`);
-    }
 
-    // Send the output of the C++ program back to the frontend
-    res.send(`Program Output: ${stdout}`);
+  if (!query) {
+    return res.status(400).send("Query word is required");
+  }
+
+  // Create a connection to the C++ server
+  const client = new net.Socket();
+  client.connect(8080, "127.0.0.1", () => {
+    console.log("Connected to C++ server");
+    client.write(query); // Send the query to the C++ server
+  });
+
+  client.on("data", (data) => {
+    console.log("Received from C++:", data.toString());
+    res.send(`Program Output: ${data.toString()}`);
+    client.destroy(); // Close the connection after receiving the response
+  });
+
+  client.on("error", (err) => {
+    console.error(`Error: ${err.message}`);
+    res.status(500).send(`Error: ${err.message}`);
   });
 });
 
